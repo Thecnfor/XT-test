0b4f1619848920651f63f53a1fd5e132a93eb342
“Thecnfor” <w5555wdnmd@gmail.com>
2025-07-19 14:37:39 +0800

    123

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/header';
 
 export default function Home() {
 
   return (
-    <div className="flex flex-col h-screen max-w-3xl mx-auto p-4">
-
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+import "@/styles/components/header.scss"
+
+
+export default function Header(){
+    return(
+        <>
+            <div>
+                XT
+            </div>
+        </>
+    )
+}
