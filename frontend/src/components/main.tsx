0228736af234982b38233c56fe352b0c86ff9001
“Thecnfor” <w5555wdnmd@gmail.com>
2025-07-21 21:16:42 +0800

    feat: 更新首页布局和样式，添加动画组件和响应式设计
    - 重构首页布局，添加TextAnimate动画组件
    - 新增Marquee跑马灯组件和PlaceholdersAndVanishInput输入组件
    - 优化全局样式和响应式设计
    - 更新页面元数据和项目名称
    - 移除未使用的组件和文档

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -1,10 +1,34 @@
 'use client'
 import "@/styles/components/mainPage.scss"
+import { TextAnimate } from "@/components/magicui/text-animate";
 
 export default function Main() {
   return (
     <main>
+      <div className="main">
+        <div className="main-container">
+            <div className="main-content">
+              <TextAnimate animation="blurInUp" by="character">
+                解决日常问题++
+              </TextAnimate>
+              <div className="main-content-desc">
+                <TextAnimate animation="fadeIn" by="line" as="p">
+                  {'解锁高效办公技巧\n\nAI实用指南'}
+                </TextAnimate>
+              </div>
+            </div>
+            <div className="main-container-marquee">
 
+            </div>
+        </div>
+      </div>
+      <div className="content">
+          <div className="content-desc">
+            <TextAnimate animation="blurInUp" by="character">
+              测试阶段
+            </TextAnimate>
+          </div>
+      </div>
     </main>
   )
 }
